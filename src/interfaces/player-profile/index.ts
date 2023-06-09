499c99eef5463fd80b63b922e1485a2ff9e0612d
roq-ai <savin171099@gmail.com>
2023-06-09 13:45:08 +0000

    Initial commit

---
+++ src/interfaces/player-profile/index.ts
@@ -0,0 +1,19 @@
+import { PlayerInterface } from 'interfaces/player';
+import { GetQueryInterface } from 'interfaces';
+
+export interface PlayerProfileInterface {
+  id?: string;
+  player_id: string;
+  notes?: string;
+  created_at?: Date | string;
+  updated_at?: Date | string;
+
+  player?: PlayerInterface;
+  _count?: {};
+}
+
+export interface PlayerProfileGetQueryInterface extends GetQueryInterface {
+  id?: string;
+  player_id?: string;
+  notes?: string;
+}
---
+++ src/validationSchema/player-profiles/index.ts
@@ -0,0 +1,6 @@
+import * as yup from 'yup';
+
+export const playerProfileValidationSchema = yup.object().shape({
+  notes: yup.string(),
+  player_id: yup.string().nullable().required(),
+});
